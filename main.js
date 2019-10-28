109e8bf6d5460dd76ef095143f8d408cfb3d1037
Márton Salomváry <salomvary@gmail.com>
2019-10-28 21:10:11 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  systemPreferences,
+  dialog
+} = require("electron");
+
+app.on("ready", () => {
+  const isTrusted = systemPreferences.isTrustedAccessibilityClient(false);
+  console.log("isTrustedAccessibilityClient", isTrusted);
+  if (!isTrusted) {
+    const clickedButton = dialog.showMessageBox(null, {
+      type: 'warning',
+      message: 'Turn on accessibility',
+      detail: 'Turn on Media Keys?',
+      defaultId: 1,
+      cancelId: 0,
+      buttons: ['Not Now', 'Turn On Accessibility']
+    })
+    if (clickedButton === 1) {
+      // Calling isTrustedAccessibilityClient with prompt=true has the side effect
+      // of showing the native dialog that either denies access or opens System
+      // Preferences.
+      systemPreferences.isTrustedAccessibilityClient(true)
+    }
+
+  }
+
+
+  globalShortcut.register("MediaPlayPause", () => {
+    console.log("MediaPlayPause pressed");
+  });
+  console.log("Registered MediaPlayPause");
+
+  const win = new BrowserWindow();
+  win.loadURL("http://example.com");
+});
